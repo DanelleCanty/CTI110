2ac4fb4a04ea128daadc753c98df1b0fc1b05e14
DanelleCanty <cantyd1193@student.faytechcc.edu>
2026-06-09 14:22:42 -0400

    Delete p1lab1cantydanelle.py

--- p1lab1cantydanelle.py
+++
@@ -1,9 +0,0 @@
-# Danelle Canty
-# June 09 2026
-# P1Lab1
-# Create a program that allows user to enter their first and last name
-
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-
-print("Hello,",first_name,last_name +"!","Welcome to CTI-110!")
