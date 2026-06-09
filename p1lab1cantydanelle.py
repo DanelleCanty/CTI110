b89e4940b3f2950f00859c6843e740913caf769a
DanelleCanty <cantyd1193@student.faytechcc.edu>
2026-06-09 14:04:01 -0400

    First Python assignment
    Taking input from user and printing output.

---
+++ p1lab1cantydanelle.py
@@ -0,0 +1,9 @@
+# Danelle Canty
+# June 09 2026
+# P1Lab1
+# Create a program that allows user to enter their first and last name
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,",first_name,last_name +"!","Welcome to CTI-110!")
